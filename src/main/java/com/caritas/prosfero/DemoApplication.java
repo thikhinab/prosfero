a9211eee3a932cef3e973807b76368bf8c1b8e10
manu2002g <65400621+manu2002g@users.noreply.github.com>
2021-05-27 00:07:59 +0800

    Add files via upload

---
+++ src/main/java/com/caritas/prosfero/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.caritas.prosfero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/caritas/prosfero/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.caritas.prosfero.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
